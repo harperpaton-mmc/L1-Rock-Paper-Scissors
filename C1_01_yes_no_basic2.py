3df0582a0bc891cff91d5021dbd373a956744a20
harperpaton-mmc <harperpaton@mmc.school.nz>
2026-05-15 14:24:48 +1200

    Looping yes / no checker

--- C1_01_yes_no_basic2.py
+++ C1_01_yes_no_basic2.py
@@ -1,9 +1,16 @@
-want_instructions =input("Do you want to see the instructions? ").lower()
 
-# check to see if the user says yes / no
-if want_instructions == "yes" or want_instructions == "y":
-    print("you ")
-elif want_instructions == "no" or want_instructions == "n":
-    print("fair")
-else:
-    print("please enter yes / no")
+while True:
+    want_instructions =input("Do you want to see the instructions? ").lower()
+
+    # check to see if the user says yes / no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you ")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("fair")
+        break
+    else:
+        print("please enter yes / no")
+        continue
+
+print("we're done")
